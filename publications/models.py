16566b5a859944dd4c7808e43cd9c83c75d6f5d1
Emmanuel <E.Chamilakis@let.ru.nl>
2020-02-05 14:16:53 +0100

    Cleaned models.py and added documentation

--- publications/models.py
+++ publications/models.py
@@ -3,9 +3,6 @@ from enum import Enum
 from django import forms
 import datetime
 from django.core.validators import MaxValueValidator, MinValueValidator
-from django_countries.fields import CountryField
-from django_countries import countries
-from smart_selects.db_fields import ChainedForeignKey
 from countries_plus.models import Country
 
 MINIMUM_YEAR = 1850
@@ -13,7 +10,7 @@ MINIMUM_YEAR_PUBLICATION = 1970
 
 class FormOfPublication(models.Model):
     ''''
-
+    Manytomany field that contains only a character field of a name.
     '''
     name = models.CharField(max_length=100, blank=True)
 	
@@ -21,40 +18,66 @@ class FormOfPublication(models.Model):
         return 'Form of Publication: ' + self.name
 
 class WritingDirection(Enum):
+    '''
+    Enum class for lnaguage writing direction
+    '''
     L = "Left"
     R = "Right"
 
 class LocationType(Enum):
+    '''
+    Location enum with three different values.
+    CITY, COUNTRY, AREA
+    '''
     CITY  = "CI"
     COUNTRY = "CO"
     AREA = "AR"
 
 class DocumentType(Enum):
+    '''
+    Document type enum with four values:
+    PDF, webpage, image, video.
+    '''
     PDF = "P"
     WEBPAGE = "W"
     IMAGE = "I"
     VIDEO = "V"
 
 class Genre(models.Model):
+    '''
+    Manytomany field class. Contains only a charfield.
+    '''
     name = models.CharField(max_length=100, blank=True)
 	
     def __str__(self):
         return 'Genre: ' + self.name
  
 def current_year():
+    '''
+    get current year
+    '''
     return datetime.date.today().year
 
 def max_value_current_year(value):
+    '''
+    Get the max value of the current year plus one
+    '''
     return MaxValueValidator(current_year()+1)(value)  
     				
 class Language(models.Model):
+    '''
+    Manytomany field class. Contains name as charfield and direction as charfield but is under the hood a enum,
+    '''
     name = models.CharField(max_length=100, blank=True)
-    #direction = models.CharField(max_length=1, choices=[(tag.value, tag) for tag in WritingDirection], default='R')
     direction = models.CharField(max_length=5, choices=[(tag.name, tag.value) for tag in WritingDirection])
     def __str__(self):
         return 'name: ' + self.name + ' direction: ' + str(self.direction)
 
 class Author(models.Model):
+    '''
+    Manytomany field class with firstname and lastname as charfields.
+    Year of birth as integer between 1850 and current year.
+    '''
     firstname = models.CharField(max_length=100, blank=True)
     lastname = models.CharField(max_length=100, blank=True)
     year_of_birth = models.IntegerField(('year_of_birth'), blank=True, null=True, validators=[MinValueValidator(MINIMUM_YEAR), max_value_current_year])
@@ -63,6 +86,9 @@ class Author(models.Model):
         return 'firstname: ' + self.firstname + ' lastname: ' + self.lastname + ' date of birth: ' + str(self.year_of_birth)
 
 class Translator(models.Model):
+    '''
+    Manytomany field class with two fields firstname and lastname both charfields.
+    '''
     firstname = models.CharField(max_length=100, blank=True)
     lastname = models.CharField(max_length=100, blank=True)
    
@@ -70,6 +96,9 @@ class Translator(models.Model):
         return 'firstname: ' + self.firstname + ' lastname: ' + self.lastname
 
 class Location(models.Model):
+    '''
+    Manytomany field class with two fields name and type both are charfields.
+    '''
     name = models.CharField(max_length=100, blank=True)
     type = models.CharField(max_length=2, choices=[(tag.value, tag) for tag in LocationType], default='CI')
     
@@ -77,24 +106,36 @@ class Location(models.Model):
         return self.name
      
 class Owner(models.Model):
+    '''
+    Manytomany field class with just one charfield name
+    '''
     name = models.CharField(max_length=100, blank=True)
     
     def __str__(self):
         return self.name
 
 class IllustrationLayoutType(models.Model):
+    '''
+    Manytomany field class with just one charfield name
+    '''
     name = models.CharField(max_length=100, blank=True)
     
     def __str__(self):
         return self.name
 
 class SpecialOccasion(models.Model):
+    '''
+    Manytomany field class with just one charfield name
+    '''
     name = models.CharField(max_length=100, blank=True)
     
     def __str__(self):
         return self.name
 
 class Church(models.Model):
+    '''
+    Manytomany field class with just one charfield name
+    '''
     name = models.CharField(max_length=100, blank=True)
     
     class Meta:
@@ -103,33 +144,18 @@ class Church(models.Model):
         return self.name
 
 def year_choices():
+    '''
+    Get all years between 1850 and current year plus one.
+    '''
     return [(r,r) for r in range(MINIMUM_YEAR, datetime.date.today().year+1)]
 
 class MyForm(forms.ModelForm):
     year = forms.TypedChoiceField(coerce=int, choices=year_choices, initial=current_year)
 
 
-countries_list = [(x,y) for (x, y) in countries]
-
-
-'''
-class Country(models.Model):
-    name = models.CharField(max_length=2, choices=countries_list, blank=True)
-
-    def __unicode__(self):
-        return self.country
-        
-    class Meta:
-        verbose_name_plural = "countries"
-
-    def __str__(self):
-        return self.name
-'''
 class City(models.Model):
     name = models.CharField(max_length=255, blank=True)
     country = models.ForeignKey(Country, related_name='cities', on_delete=models.CASCADE)
-    #country = models.CharField(max_length=2, choices=countries_list, blank=True)
-        #models.ForeignKey('Country', related_name="cities", on_delete=models.CASCADE)
 
     class Meta:
         verbose_name_plural = "cities"
@@ -138,17 +164,23 @@ class City(models.Model):
         return self.name  
         
 class UploadedFile(models.Model):
+    '''
+    Manytomany field class with three fields.
+    A charfield, filefield and DateTimeField.
+    The DateTimeField will be automatically add.
+    '''
     description = models.CharField(max_length=255, blank=True)
     file = models.FileField(upload_to='files', blank=True, null=True)
     uploaded_at = models.DateTimeField(auto_now_add=True, blank=True, null=True)
 
     def __str__(self):
-        return self.description  
-
-
-
+        return self.description
 
 class Publication(models.Model):
+    '''
+    Main class for Publications contains many charfields and several manytomany fields.
+    '''
+
     title_original = models.CharField(max_length=300, blank=True)
     title_subtitle_transcription = models.CharField(max_length=300, blank=True)
     title_subtitle_European = models.CharField(max_length=300, blank=True)
@@ -159,22 +191,8 @@ class Publication(models.Model):
     printed_by = models.CharField(max_length=100, blank=True)
     published_by = models.CharField(max_length=100, blank=True)
     publication_date = models.CharField(max_length=100, blank=True)
-    #publication_country = models.ManyToManyField(Country)
-    #publication_country = models.CharField(max_length=2, choices=countries_list, blank=True)
-    #publication_country = CountryField(blank=True, null=True)
     publication_country = models.ForeignKey(Country, on_delete=models.CASCADE, null=True, blank=True)
     publication_city = models.ForeignKey(City, on_delete=models.CASCADE, null=True, blank=True)
-    '''
-    publication_city = ChainedForeignKey(
-        City,
-        chained_field="publication_country",
-        chained_model_field="country",
-        show_all=False,
-        auto_choose=True,
-        sort=True, 
-        blank=True,
-        null=True)
-    '''
     publishing_organisation = models.CharField(max_length=100, blank=True)
     possible_donor = models.CharField(max_length=100, blank=True)
     affiliated_church = models.ManyToManyField(Church)
@@ -186,7 +204,6 @@ class Publication(models.Model):
     image_details = models.CharField(max_length=300, blank=True)
     nr_of_pages = models.IntegerField(blank=True, null=True)
     collection_date = models.CharField(max_length=100, blank=True)
-    #collection_country = CountryField(blank=True)
     collection_country = models.ForeignKey(Country, on_delete=models.CASCADE, related_name='collections', null=True, blank=True)
     collection_venue_and_city = models.CharField(max_length=100, blank=True)
     copyrights = models.NullBooleanField()
@@ -207,14 +224,3 @@ class Publication(models.Model):
         return 'title_original: ' + self.title_original +',  title_subtitle_transcription ' + self.title_subtitle_transcription\
                 + ', title_subtitle_European: ' + self.title_subtitle_European + ', title_translation: ' + self.title_translation
 
-'''
-    def create_countries(self):
-        countries_list = [y for (x, y) in countries]
-
-        for country in countries_list:
-            c = Country(name=country)
-            c.save()
-            for pub in Publication.objects.all():
-                pub.publication_country.add(c)
-                pub.save()
-'''
